2a5e0eae56bd56795ecdbaa0c18e49a86e6b42a8
DinnoKoluh <koluh.dinno@gmail.com>
2021-03-02 01:09:05 +0100

    Started creating Blog project

---
+++ djangoStuff/blog_project/mysite/blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+#---------------
+from django.utils import timezone
+from django.urls import reverse
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # this is not gonna be the usual case when dealing with apps which have multiuser .../ connected to a super user on the web site
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    # connected to the related_name = comments from the Comment class
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    #after an instance of this post is created, where should the website take us
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+    
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE) # each comment is gonna be connected to a blog (with the Foreign key)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
