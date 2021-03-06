2023e171ed925048e082c21e8c783e17e09a1536
DinnoKoluh <koluh.dinno@gmail.com>
2021-03-06 23:11:43 +0100

    Done work on posts in the clone project

--- djangoStuff/clone_project/simplesocial/accounts/admin.py
+++ djangoStuff/clone_project/simplesocial/accounts/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from . import models
-# Register your models here.
-# No need to register models beacues we use Django's built-in models
-class GroupMemberInline(admin.TabularInline):
-    model = models.GroupMember
-
-admin.site.register(models.Group)
--- djangoStuff/clone_project/simplesocial/groups/urls.py
+++ djangoStuff/clone_project/simplesocial/groups/urls.py
@@ -6,6 +6,6 @@ app_name = 'groups' # to call it from .html files
 urlpatterns = [
     path('', views.ListGroups.as_view(), name = 'all'),
     path('new', views.CreateGroup.as_view(), name='create'),
-    path('postst/in/<slug:slug>', views.SingleGroup.as_view(), name='single'),
+    path('posts/in/<slug:slug>', views.SingleGroup.as_view(), name='single'),
     
 ]
